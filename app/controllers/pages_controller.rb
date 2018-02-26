2866cda1f5ce82de0e99ef18327afbb7f21f1b5e
Alex Shapiro <acjshapiro@gmail.com>
2018-02-26 12:10:01 -0700

    create pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,31 @@
+class PagesController < ApplicationController
+
+#GET /pages
+  def index
+    @pages = Page.all
+  end
+#GET /pages/:id
+  def show
+    @page = Page.find(params[:id])
+  end
+#GET /pages/new  load a form to fill out
+  def new
+    @page = Page.new
+  end
+#POST /pages
+  def create
+    @page = Page.new(page_params)
+    if @page.save
+      redirect_to page_path(@page)
+    else
+      render :new
+    end
+  end
+
+  private
+    def page_params
+      params.require(:page).permit(:body, :author, :title)
+    end  
+
+
+end
